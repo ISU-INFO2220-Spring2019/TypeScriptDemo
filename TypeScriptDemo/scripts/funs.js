c4788aca3b1296cced0985e2b43641f5a88f9617
Jon Holmes <holmjona@isu.edu>
2019-04-17 12:21:25 -0600

    TypeScript Classes and Functions

--- TypeScriptDemo/scripts/funs.js
+++ TypeScriptDemo/scripts/funs.js
@@ -1,1 +1,76 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    }
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Thing = /** @class */ (function () {
+    function Thing() {
+    }
+    Thing.prototype.fun = function (x, y, z) {
+        return x === y;
+    };
+    return Thing;
+}());
+var t;
+t = new Thing();
+t.fun(1, "2");
+t.fun(1, 2, 3);
+var Person = /** @class */ (function () {
+    function Person(fName, lName) {
+        this._FirstName = fName;
+        this._LastName = lName;
+    }
+    Object.defineProperty(Person.prototype, "firstName", {
+        get: function () {
+            return this._FirstName;
+        },
+        set: function (a) {
+            this._FirstName = a;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Person.prototype, "happy", {
+        get: function () {
+            return "happiness";
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Person;
+}());
+var p = new Person("Bob", "Wooley");
+var f = p.firstName;
+p.firstName = "Bully";
+var Employee = /** @class */ (function (_super) {
+    __extends(Employee, _super);
+    function Employee(wge) {
+        var _this = _super.call(this, "Jane", "Doe") || this;
+        _this.wage = wge;
+        return _this;
+    }
+    Object.defineProperty(Employee.prototype, "wage", {
+        get: function () {
+            return this._Wage;
+        },
+        set: function (val) {
+            this._Wage = val;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Employee;
+}(Person));
+var e = new Employee(30);
+e.firstName = "Sally";
+//e.newMagicThing = "asdfasdf";
+this.call("HelloWorld", "bob");
 //# sourceMappingURL=funs.js.map
--- TypeScriptDemo/scripts/funs.ts
+++ TypeScriptDemo/scripts/funs.ts
@@ -1,1 +1,57 @@
-﻿
+﻿class Thing{
+    fun(x:number,y:number | string,z?:number): boolean {
+        return x === y;
+    }
+}
+var t: Thing;
+t = new Thing();
+t.fun(1, "2");
+t.fun(1, 2, 3);
+
+class Person{
+    private _FirstName: string;
+    private _LastName: string;
+
+    constructor(fName: string, lName: string) {
+        this._FirstName = fName;
+        this._LastName = lName;
+    }
+
+    get firstName() {
+        return this._FirstName;
+    }
+    set firstName(a:string) {
+        this._FirstName = a;
+    }
+    get happy() {
+        return "happiness";
+    }
+}
+
+var p = new Person("Bob","Wooley");
+var f = p.firstName;
+p.firstName = "Bully";
+
+class Employee extends Person {
+    private _Wage: number;
+
+    constructor(wge: number) {
+        super("Jane", "Doe");
+        this.wage = wge;
+    }
+
+    get wage() {
+        return this._Wage;
+    }
+    set wage(val: number) {
+        this._Wage = val;
+    }
+
+}
+
+var e: Employee = new Employee(30);
+e.firstName = "Sally";
+//e.newMagicThing = "asdfasdf";
+
+this.call("HelloWorld","bob")
+
